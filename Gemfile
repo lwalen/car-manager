8bb51b365875f95c3e79948c0bbce03224922175
Lars Walen <lars@walen.me>
2013-08-01 14:32:30 -0600

    Changed skeleton over to plain old files

--- Gemfile
+++ Gemfile
@@ -45,5 +45,4 @@ gem 'bcrypt-ruby', '~> 3.0.0'
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
-gem "skeleton-rails"
 gem "haml"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
 	def cancel_link
-		return link_to 'Cancel', request.env["HTTP_REFERER"], 
-			:class => 'btn'
+		return link_to 'Cancel', request.env["HTTP_REFERER"]
 	end
 end
