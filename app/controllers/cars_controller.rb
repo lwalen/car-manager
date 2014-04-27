8a5c76a6ab024261ae6aa5b8957c18f3f93e1e4d
Lars Walen <lars@walen.me>
2014-04-26 19:42:55 -0700

    Changed order query for records

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,7 +5,7 @@ class CarsController < ApplicationController
 
 	def show
 		@car = Car.find(params[:id])
-		@records = @car.records.find(:all, order: "mileage")
+		@records = @car.records.order(:mileage)
 
 		respond_to do |format|
 			format.html
