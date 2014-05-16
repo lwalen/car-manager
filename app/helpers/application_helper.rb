9bf78eb9fb46c05c810b2cb182f88c1d3e7a1e9c
Lars Walen <lars@walen.me>
2014-05-15 21:31:47 -0700

    Made show all link more obvious, and simpler

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,4 @@ module ApplicationHelper
 	def cancel_link
 		return link_to 'Cancel', request.env["HTTP_REFERER"]
 	end
-
-  def add_s value
-    if value != 1
-      return 's'
-    end
-  end
 end
