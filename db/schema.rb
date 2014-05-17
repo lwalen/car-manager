117b0646a99c4efbe397b01e6411dc7600ee83db
Lars Walen <lars@walen.me>
2014-05-16 19:39:09 -0700

    Migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130808194630) do
+ActiveRecord::Schema.define(version: 20140517023605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,23 @@ ActiveRecord::Schema.define(version: 20130808194630) do
     t.datetime "updated_at"
     t.float    "mpg"
     t.boolean  "stat",       default: true, null: false
+  end
+
+  create_table "service_types", force: true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "services", force: true do |t|
+    t.integer  "car_id"
+    t.date     "date"
+    t.integer  "mileage"
+    t.integer  "type"
+    t.text     "notes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
