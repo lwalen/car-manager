3bc8c0a5d87b47d8291eb79c77c6497d9221580f
Lars Walen <lars@walen.me>
2014-05-17 12:27:08 -0700

    Fixed indentation

--- app/assets/javascripts/cars.js
+++ app/assets/javascripts/cars.js
@@ -17,11 +17,11 @@ $(function() {
 	var hash = document.location.hash;
 	var prefix = "tab_";
 	if (hash) {
-	    $('.nav-tabs a[href='+hash.replace(prefix,"")+']').tab('show');
+		$('.nav-tabs a[href='+hash.replace(prefix,"")+']').tab('show');
 	}
 
 	// Change hash for page-reload
 	$('.nav-tabs a').on('shown.bs.tab', function (e) {
-	    window.location.hash = e.target.hash.replace("#", "#" + prefix);
+		window.location.hash = e.target.hash.replace("#", "#" + prefix);
 	});
 });
