e3c1dc023f3b35101c8587ce7c5ced8b97d8dbe5
Lars Walen <lars@walen.me>
2014-05-18 17:32:50 -0700

    Can show action buttons on mobile by tapping row

--- app/assets/javascripts/records.js
+++ app/assets/javascripts/records.js
@@ -1,12 +1,32 @@
 
 $(function() {
 
-	$('table tbody tr').on({
+	var width = $(window).width();
+
+	$('table.records tbody tr').on({
 		mouseover: function() {
-			$(this).children().children('.actions').children().css('visibility', 'visible');
+			$(this).children('.actions').children('a').css('visibility', 'visible');
 		},
 		mouseout: function() {
-			$(this).children().children('.actions').children().css('visibility', 'hidden');
+			$(this).children('.actions').children('a').css('visibility', 'hidden');
+		},
+		click: function() {
+
+			if (width < 768) {
+
+				var actions = $(this).children('.actions');
+				if (actions.hasClass('hidden-xs')) {
+					$(this).children('.mileage, .gallons, .cost').hide();
+					$(actions).children('a').css('visibility', 'visible');
+					$(actions).attr('colspan', 3);
+					$(actions).removeClass('hidden-xs');
+				} else {
+					$(actions).addClass('hidden-xs');
+					$(actions).attr('colspan', 1);
+					$(actions).children('a').css('visibility', 'hidden');
+					$(this).children('.mileage, .gallons, .cost').show();
+				}
+			}
 		}
 	});
 });
